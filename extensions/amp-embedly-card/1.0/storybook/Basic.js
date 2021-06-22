2e8d07d78cd738fcba72b95df249ecd75d86cc93
AnuragVasanwala <AnuragVasanwala@gmail.com>
2021-06-22 12:13:54 +0530

    ♻️ Updated version, Minor bug fix

--- extensions/amp-embedly-card/1.0/storybook/Basic.js
+++ extensions/amp-embedly-card/1.0/storybook/Basic.js
@@ -26,12 +26,15 @@ export default {
 
 export const _default = () => {
   return (
-    <EmbedlyCard
-      bootstrap="http://localhost:9002/dist.3p/current/vendor/embedly.max.js"
-      src="http://ads.localhost:9002/dist.3p/current/frame.max.html"
-      options={{url: 'https://www.youtube.com/watch?v=lBTCB7yLs8Y'}}
-      title="Embedly Card"
-      style={{width: '400px', height: '400px'}}
-    />
+    <>
+      Hello World
+      <EmbedlyCard
+        bootstrap="http://localhost:9002/dist.3p/current/vendor/embedly.max.js"
+        src="http://ads.localhost:9002/dist.3p/current/frame.max.html"
+        url="https://www.youtube.com/watch?v=lBTCB7yLs8Y"
+        title="Embedly Card"
+        style={{width: '400px', height: '400px'}}
+      />
+    </>
   );
 };
